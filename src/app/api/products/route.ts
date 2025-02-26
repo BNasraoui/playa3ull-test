b5344a59437a26bfc3891ba5de8cc4fe6fb281b2
ben-zeligate <nasraouiprojects@gmail.com>
2025-02-26 23:09:48 +1000

    update route

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    // Replace with your external API URL.
     const res = await fetch("https://fakestoreapi.com/products");
     if (!res.ok) {
       return NextResponse.error();
